630c1858f10eb02ccbfeb1cb1687eb2a286a92db
Alex Badia <badia.alex.h@gmail.com>
2021-04-16 17:03:42 -0400

    implmented allowing the user to click one at a time the cst. There were a few ways I could've gone about it, but chose to let the user highlight multiple different sub branches

--- docs/distrib/controller/compiler_controller/compiler_controller.js
+++ docs/distrib/controller/compiler_controller/compiler_controller.js
@@ -60,41 +60,72 @@ var NightingaleCompiler;
             let stacktrace_console_model = new NightingaleCompiler.StacktraceConsoleModel(this.lexer.stacktrace_stack);
             let footer_model = new NightingaleCompiler.FooterModel(this.lexer.errors_stream.length, this.lexer.warnings_stream.length);
         } // compilerControllerBtnCompile_click
+        /**
+         * Highlights the current node clicked on and all of the node's descendants
+         *
+         * Too much recursive brain damage, thus implemented iteratively
+         * @param program_num
+         * @param node_id
+         */
         static compilerControllerBtnLightUpTree_click(program_num, node_id) {
-            //  TODO: Fix this, not important though
-            // let stack: Array<HTMLCollection> = [];
-            // let current_node = document.getElementById(`p${program_num}_li_node_id_${node_id}`);
-            // stack.push(current_node.children);
-            // if (current_node.classList.contains("node_active")) {
-            //     current_node.classList.remove("node__active");
-            //     let children = current_node.children;
-            //     while (stack.length != 0) {
-            //         let currentItemInStack = stack.pop();
-            //         for (let child: number = 0; child < currentItemInStack.length; ++child) {
-            //             children[child].classList.remove("node__active");
-            //             let nestedChildren = children[child].children;
-            //             // Prevents infinite stack hopefully... I hatre recursion...
-            //             if (nestedChildren != undefined && nestedChildren != null && nestedChildren.length > 0) {
-            //                 stack.push(children[child].children);
-            //             }// if
-            //         }// for
-            //     }// while
-            // }// if
-            // else {
-            //     current_node.classList.remove("node__active");
-            //     let children = current_node.children;
-            //     while (stack.length != 0) {
-            //         let currentItemInStack = stack.pop();
-            //         for (let child: number = 0; child < currentItemInStack.length; ++child) {
-            //             children[child].classList.add("node__active");
-            //             let nestedChildren = children[child].children;
-            //             // Prevents infinite stack hopefully... I hatre recursion...
-            //             if (nestedChildren != undefined && nestedChildren != null && nestedChildren.length > 0) {
-            //                 stack.push(children[child].children);
-            //             }// if
-            //         }// for
-            //     }// while
-            // }// else
+            // To simulate recursion, iteratively, use a stack.
+            let stack = [];
+            // Get the starting node from DOM
+            let current_node = document.getElementById(`p${program_num}_li_node_id_${node_id}`);
+            // Push starting node's children onto stack
+            let children = current_node.children;
+            stack.push(children);
+            // Starting node is already highlighted, thus unhighlight starting node and it's descendants.
+            if (children.namedItem("node-anchor-tag").classList.contains("anchor-node__active")) {
+                if (current_node instanceof HTMLAnchorElement) {
+                    // Remove the CSS class that highlights the node
+                    current_node.classList.remove("anchor-node__active");
+                } // if
+                while (stack.length > 0) {
+                    // Get current nodes children elements from the stack
+                    let currentRemoveItemInStack = stack.pop();
+                    // Remove highlight from each child
+                    for (let removeChild = 0; removeChild < currentRemoveItemInStack.length; ++removeChild) {
+                        // Only remove highlight from links <a> and <li>
+                        if (currentRemoveItemInStack[removeChild] instanceof HTMLAnchorElement) {
+                            if (currentRemoveItemInStack[removeChild].classList.contains("anchor-node__active")) {
+                                currentRemoveItemInStack[removeChild].classList.remove("anchor-node__active");
+                            } // if
+                        } // if
+                        let nestedChildren = currentRemoveItemInStack[removeChild].children;
+                        // Prevents infinite stack hopefully... I hatre recursion...
+                        if (nestedChildren !== undefined && nestedChildren !== null && nestedChildren.length > 0) {
+                            stack.push(nestedChildren);
+                        } // if
+                    } // for
+                } // while
+            } // if
+            // Highlight current nod and its descendants
+            else {
+                if (current_node instanceof HTMLAnchorElement) {
+                    // Add the CSS class that highlights the node
+                    current_node.classList.add("anchor-node__active");
+                } // if
+                while (stack.length > 0) {
+                    // Get current nodes children elements from the stack
+                    let currentAddItemInStack = stack.pop();
+                    // Add highlight from each child
+                    for (let addChild = 0; addChild < currentAddItemInStack.length; ++addChild) {
+                        console.log(currentAddItemInStack[addChild]);
+                        // Only remove highlight from links <a> and <li>
+                        if (currentAddItemInStack[addChild] instanceof HTMLAnchorElement) {
+                            if (!currentAddItemInStack[addChild].classList.contains("anchor-node__active")) {
+                                currentAddItemInStack[addChild].classList.add("anchor-node__active");
+                            } // if
+                        } // if
+                        let nestedChildren = currentAddItemInStack[addChild].children;
+                        // Prevents infinite stack hopefully... I hatre recursion...
+                        if (nestedChildren !== undefined && nestedChildren !== null && nestedChildren.length > 0) {
+                            stack.push(nestedChildren);
+                        } // if
+                    } // for
+                } // while
+            } // else
             // // background: #c8e4f8; color: #000; border: 1px solid #94a0b4;
             // current_node.style.backgroundColor = "#c8e4f8";
             // current_node.style.color = "#000000";
@@ -103,4 +134,46 @@ var NightingaleCompiler;
     } // class
     NightingaleCompiler.CompilerController = CompilerController;
 })(NightingaleCompiler || (NightingaleCompiler = {})); // module
+// Stack to store the nodes
+// let nodes: Array<Node> = [];
+// // push the current node onto the stack
+// nodes.push(root);
+// // Loop while the stack is not empty
+// while (nodes.length !== 0) {
+//     // Store the current node and pop
+//     // it from the stack
+//     let curr: Node = nodes.pop();
+//     // Current node has been travarsed
+//     if (curr != null) {
+//         // Root node
+//         if (curr.parent_node == null) {
+//             // Root node already created
+//             ///console.log(`Current: ${curr.name} | ${curr.id}, Parent: ${curr.parent_node.id}`);
+//         }// if
+//         // Node is the first node of the parent
+//         else if (curr.parent_node.children_nodes[0] == curr) {
+//             console.log(`Current: ${curr.name} | ${curr.id}, Parent: ${curr.parent_node.id}, 1st child`);
+//             let ul: HTMLUListElement = document.createElement("ul");
+//             ul.id = `p${this._program}_ul_node_id_${curr.id}`;
+//             let li: HTMLLIElement = document.createElement("li");
+//             li.id = `p${this._program}_li_node_id_${curr.id}`;
+//             ul.appendChild(li);
+//             li.innerHTML = `<a onclick="NightingaleCompiler.CompilerController.compilerControllerBtnLightUpTree_click(${this._program}, ${curr.id});" >${curr.name}</a>`;
+//             document.getElementById(`p${this._program}_li_node_id_${curr.parent_node.id}`).appendChild(ul);
+//         }// if
+//         // Node is 2nd or 3rd or nth child of parent
+//         else {
+//             console.log(`Current: ${curr.name} | ${curr.id}, Parent: ${curr.parent_node.id}, ul ${curr.parent_node.children_nodes[0].id}`);
+//             let li: HTMLLIElement = document.createElement("li");
+//             li.id = `p${this._program}_li_node_id_${curr.id}`;
+//             li.innerHTML = `<a>${curr.name}</a>`;
+//             document.getElementById(`p${this._program}_ul_node_id_${curr.parent_node.children_nodes[0].id}`).appendChild(li);
+//         }// else
+//         // Store all the children of 
+//         // current node from right to left.
+//         for (let i: number = curr.children_nodes.length - 1; i >= 0; --i) {
+//             nodes.push(curr.children_nodes[i]);
+//         }// for
+//     }
+// }
 //# sourceMappingURL=compiler_controller.js.map
--- docs/src/model/concrete_syntax_tree_model.ts
+++ docs/src/model/concrete_syntax_tree_model.ts
@@ -193,7 +193,7 @@ module NightingaleCompiler {
 
                         ul.appendChild(li);
 
-                        li.innerHTML = `<a onclick="NightingaleCompiler.CompilerController.compilerControllerBtnLightUpTree_click(${this._program}, ${curr.id});" >${curr.name}</a>`;
+                        li.innerHTML = `<a onclick="NightingaleCompiler.CompilerController.compilerControllerBtnLightUpTree_click(${this._program}, ${curr.id});" name = "node-anchor-tag" >${curr.name}</a>`;
 
                         document.getElementById(`p${this._program}_li_node_id_${curr.parent_node.id}`).appendChild(ul);
                     }// if
@@ -203,7 +203,7 @@ module NightingaleCompiler {
                         console.log(`Current: ${curr.name} | ${curr.id}, Parent: ${curr.parent_node.id}, ul ${curr.parent_node.children_nodes[0].id}`);
                         let li: HTMLLIElement = document.createElement("li");
                         li.id = `p${this._program}_li_node_id_${curr.id}`;
-                        li.innerHTML = `<a>${curr.name}</a>`;
+                        li.innerHTML = `<a onclick="NightingaleCompiler.CompilerController.compilerControllerBtnLightUpTree_click(${this._program}, ${curr.id});" name = "node-anchor-tag">${curr.name}</a>`;
 
                         document.getElementById(`p${this._program}_ul_node_id_${curr.parent_node.children_nodes[0].id}`).appendChild(li);
                     }// else
